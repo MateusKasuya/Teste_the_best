789e5d07a9d71f47a187b1868e73dd2b1ed6a31b
Mateus Kasuya <mateusvbkasuya@gmail.com>
2024-08-11 15:20:04 -0300

    build: schema pandera

--- src/schema.py
+++ src/schema.py
@@ -1,29 +1,44 @@
-from pydantic import BaseModel, PositiveInt, EmailStr, PositiveFloat
-from datetime import datetime
-from typing import Literal
+import pandera as pa
+from pandera.typing import DataFrame, Series
+import pandas as pd
 
+email_regex = r"[^@]+@[^@]+\.[^@]+"
 
-class Cliente(BaseModel):
-    cliente_id : PositiveInt
-    nome : str
-    email : EmailStr
-    data_cadastro : datetime
+class Cliente(pa.SchemaModel):
+    cliente_id : Series[int] = pa.Field(gt = 0)
+    nome : Series[str]
+    email : Series[str] = pa.Field(regex = email_regex)
+    data_cadastro  : Series[pd.Timestamp] = pa.Field()
 
-class Produtos(BaseModel):
-    produto_id : PositiveInt
-    nome : str
-    categoria : Literal['Alimentos', 'Complementos', 'Bebidas', 'Snacks']
-    preco : PositiveFloat
+    class Config:
+        coerce = True
+        strict = True
 
-class Pedidos(BaseModel):
-    pedido_id : PositiveInt
-    cliente_id : PositiveInt
-    data_pedido : datetime
-    valor_total : PositiveFloat
+CATEGORIAS_PERMITIDAS = ['Alimentos', 'Complementos', 'Bebidas', 'Snacks']
 
-class ItensPedido(BaseModel):
-    item_pedido_id : PositiveInt
-    pedido_id : PositiveInt
-    produto_id : PositiveInt
-    quantidade : PositiveInt
-    preco_unitario : PositiveFloat
+class Produtos(pa.SchemaModel):
+    produto_id : Series[int] = pa.Field(gt = 0)
+    nome : Series[str]
+    categoria : Series[str] = pa.Field(check = pa.Check(lambda x: x.isin(CATEGORIAS_PERMITIDAS)))
+    preco : Series[float] = pa.Field(gt = 0)
+
+    class Config:
+        coerce = True
+        strict = True
+
+class Pedidos(pa.SchemaModel):
+    pedido_id : Series[int] = pa.Field(gt = 0)
+    cliente_id : Series[int] = pa.Field(gt = 0)
+    data_pedido : Series[pd.Timestamp] = pa.Field()
+    valor_total : Series[float] = pa.Field(gt = 0)
+
+    class Config:
+        coerce = True
+        strict = True
+
+class ItensPedido(pa.SchemaModel):
+    item_pedido_id : Series[int] = pa.Field(gt = 0)
+    pedido_id : Series[int] = pa.Field(gt = 0)
+    produto_id : Series[int] = pa.Field(gt = 0)
+    quantidade : Series[int] = pa.Field(gt = 0)
+    preco_unitario : Series[float] = pa.Field(gt = 0)
